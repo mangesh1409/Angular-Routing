cf99902d16d46a42e7ebbba8a87cabcbb682aab6
Mangesh Jadhav <149mpj@gmail.com>
2020-07-09 00:18:03 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '', component: StudentComponent              // Dont add path in URL
+  // },
+  {
+    path: '', redirectTo: 'student', pathMatch: 'full'    // add path in URL
+  },
+  {
+    path: 'student', component: StudentComponent
+  },
+  {
+    path: 'studentdetails', component: StudentDetailsComponent
+  },
+  {
+    path: '**', component: PagenotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
