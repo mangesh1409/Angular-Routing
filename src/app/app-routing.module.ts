52ddcdb98ef68682a10910d71dbc4536d0b389ee
Mangesh Jadhav <149mpj@gmail.com>
2020-07-09 18:44:11 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { StudentComponent } from './student/student.component';
 import { StudentDetailsComponent } from './student-details/student-details.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { StudentRegComponent } from './student-reg/student-reg.component';
 
 
 const routes: Routes = [
   // {
   //   path: '', component: StudentComponent              // Dont add path in URL
   // },
+  // {
+  //   path: '', redirectTo: 'student', pathMatch: 'full'    // add path in URL
+  // },
+
   {
-    path: '', redirectTo: 'student', pathMatch: 'full'    // add path in URL
+    path: 'student',
+    children : [
+      {  path: '', component: StudentComponent  },           // Dont add path in URL
+      {  path: 'studentregistration', component: StudentRegComponent  },
+      {  path: 'studentdetails', component: StudentDetailsComponent   }
+    ]
   },
-  {
-    path: 'student', component: StudentComponent
-  },
-  {
-    path: 'studentdetails', component: StudentDetailsComponent
-  },
+
   {
     path: '**', component: PagenotfoundComponent
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
 import { StudentDetailsComponent } from './student-details/student-details.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { StudentRegComponent } from './student-reg/student-reg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
     StudentDetailsComponent,
-    PagenotfoundComponent
+    PagenotfoundComponent,
+    StudentRegComponent
   ],
   imports: [
     BrowserModule,
